097002898a5989103f5e0f3de82ff4c2cb3c9e28
seanpconkie <seanpconkie@yahoo.com>
2018-08-22 11:12:02 +0100

    Add Identity Scaffold

---
+++ GustafsGalleryStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GustafsGalleryStore.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GustafsGalleryStore.Areas.Identity.IdentityHostingStartup))]
+namespace GustafsGalleryStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<GustafsGalleryStoreContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("GustafsGalleryStoreContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<GustafsGalleryStoreContext>();
+            });
+        }
+    }
+}
